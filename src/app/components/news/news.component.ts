b955f1ef08f1277142c2c2ba705b8f3d53aaf562
Axel Guzmán <axl_bg@hotmail.com>
2024-04-06 20:14:03 -0300

    up

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
 })
 export class NewsComponent {
+  arrowBtns = document.querySelectorAll('.wrapper button');
 
+  clickIzq() {
+    document.getElementById('carro')!.scrollLeft -= 100;
+  }
+  clickDer() {
+    document.getElementById('carro')!.scrollLeft += 100;
+  }
 }
