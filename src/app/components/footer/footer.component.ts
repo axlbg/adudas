88060e7440b7cd58e79ee87eb25d6aa94efba9a6
Axel Guzmán <axl_bg@hotmail.com>
2024-04-09 14:02:24 -0300

    up footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  columnas = [
+    {
+      titulo: 'AYUDA',
+      items: ['Envíos y entregas', 'Devoluciones', 'Cambios', 'Contactarse'],
+    },
+    {
+      titulo: 'ACERCA DE',
+      items: ['Noticias', 'Inversionistas', 'Terminos y condiciones'],
+    },
+    {
+      titulo: 'NOVEDADES',
+      items: ['Lo nuevo', 'Hombres', 'Mujeres', 'Niños', 'Promociones'],
+    },
+  ];
 }
