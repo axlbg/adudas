eae6bacf9b62750f3cda3dc16d923582d8d26d8c
Axel Guzmán <axl_bg@hotmail.com>
2024-04-10 15:29:09 -0300

    up

--- src/app/components/publicidad/publicidad.component.ts
+++ src/app/components/publicidad/publicidad.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-publicidad',
   templateUrl: './publicidad.component.html',
-  styleUrls: ['./publicidad.component.css']
+  styleUrls: ['./publicidad.component.css'],
 })
 export class PublicidadComponent {
-
+  mensaje = {
+    texto: 'Hasta 6 cuotas sin interés con bancos seleccionados',
+    a: 'Ver promociones bancarias',
+    href: '#',
+  };
 }
