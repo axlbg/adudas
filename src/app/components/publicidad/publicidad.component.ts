19e932fae881298d997d4275a3e9b2d6e250d346
Axel Guzmán <axl_bg@hotmail.com>
2024-04-10 16:46:22 -0300

    up

--- src/app/components/publicidad/publicidad.component.ts
+++ src/app/components/publicidad/publicidad.component.ts
@@ -6,9 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./publicidad.component.css'],
 })
 export class PublicidadComponent {
-  mensaje = {
-    texto: 'Hasta 6 cuotas sin interés con bancos seleccionados',
-    a: 'Ver promociones bancarias',
-    href: '#',
-  };
+  x = 0;
+  mensajes = [
+    {
+      texto: 'Envío gratis en muchísimos productos',
+      a: 'Ver cuáles',
+      href: '/adudas',
+    },
+    {
+      texto: 'Algunos productos pueden no ser lo que parece',
+      a: 'Ver terminos y condiciones',
+      href: '#',
+    },
+    {
+      texto: 'Tenemos la ropa de Messi',
+      a: 'Ver colección',
+      href: '/coleccion/messi',
+    },
+  ];
+
+  clickArrowLeft() {
+    this.x -= 1;
+    if (this.x < 0) {
+      this.x = this.mensajes.length - 1;
+    }
+  }
+  clickArrowRight() {
+    this.x += 1;
+    if (this.x >= this.mensajes.length) {
+      this.x = 0;
+    }
+  }
 }
